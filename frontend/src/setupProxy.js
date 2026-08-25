cf9918608061f97f0ab625ea9a315a796c83df51
nilakshee-sahu <sahunilakshee@gmail.com>
2026-08-25 11:57:35 +0530

    Update proxy target to use production URL

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://shopgrid-3joz.onrender.com/",
       changeOrigin: true,
     }),
   );
